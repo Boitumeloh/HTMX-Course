19a9922555addf6fb1c35a39193488430fb2056e
Boitumelo <boitumelolefophane@gmail.com>
2024-08-27 16:37:56 +0200

    no changes

--- Project/expense-manager/routes/index.js
+++ Project/expense-manager/routes/index.js
@@ -11,7 +11,7 @@ const expenses = [
 ];
 
 // GET /expenses
-router.get('/expenses', (req, res) => {
+router.get('/expense s', (req, res) => {
   res.render('index', { action: '', expenses, expense: {} });
 });
 
